e6b4c870c7864ab911e2122c9556c5afd8673357
Srivathsa <bssrivathsa2003@gmail.com>
2023-02-01 00:01:46 +0530

    release

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Colour {
+  Color bgcolor = const Color(0xffffffff);
+  Color actcolor = const Color(0xffEFEFEF);
+  Color orangeColor = const Color(0xffF57D31);
+  Color blueColor = const Color(0xff6493EB);
+  Color greenColor = const Color(0xff74CB48);
+  Color purpleColor = const Color(0xff70559B);
+  Color pinkColor = const Color(0xffFB5584);
+  Color yellowColor = const Color(0xffF9CF30);
+  Color poisonColor = const Color(0xffA43E9E);
+}
