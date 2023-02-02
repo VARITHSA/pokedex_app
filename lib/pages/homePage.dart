6e5b4ef6974acd22e88dfc2505d259caef2bad76
Srivathsa <bssrivathsa2003@gmail.com>
2023-02-02 16:07:47 +0530

    json data is shown in homePage

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -6,6 +6,13 @@ import 'package:flutter/services.dart' as rootBundle;
 import 'package:pokedex_app/utils/json_model.dart';
 
 class HomePage extends StatelessWidget {
+  Future<List<Data>> readJsonData() async {
+    final jsondata = await rootBundle.rootBundle.loadString('assets/data.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    return list.map((e) => Data.fromJson(e)).toList();
+  }
+
   const HomePage({super.key});
 
   @override
@@ -13,30 +20,28 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: FutureBuilder(
-        future: readJsonData(),
-        builder: ((context, snapshot) {
-          if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          } else if (snapshot.hasData) {
-            return ListView.builder(itemBuilder: ((context, index) {
-              return Card(
-                child: Container(),
+          future: readJsonData(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
               );
-            }));
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        }),
-      ),
+            } else if (snapshot.hasData) {
+              var items = snapshot.data as List<Data>;
+              return ListView.builder(
+                  itemCount: items == null ? 0 : items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child:
+                          Container(child: Text(items[index].name.toString())),
+                    );
+                  });
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          })),
     );
-  }
-
-  Future<List<Data>> readJsonData() async {
-    final jsondata = await rootBundle.rootBundle.loadString('assets/data.json');
-    final list = json.decode(jsondata) as List<dynamic>;
-
-    return list.map((e) => Data.fromJson(e)).toList();
   }
 }
